bcda4132f2cc47ce691d8000a8dc919f841b5014
Andres <aneduardotejada@gmail.com>
2021-05-01 17:37:03 -0500

    gatsby hotel 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: "/gatsby-hotel",
+  pathPrefix: "andrestejada/gatsby-hotel",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
